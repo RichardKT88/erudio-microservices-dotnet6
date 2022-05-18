a8d977ead0088eee2650c539abbf7a7247fa92de
RichardKT88 <kendy_t88@hotmail.com>
2022-05-18 16:30:14 -0300

    [Done] Seção 5: [SEÇÃO DE NIVELAMENTO] REST na Prática com ASP.NET

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithASPNETUdemy.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Address { get; set; } = String.Empty;
+        public string Gender { get; set; } = String.Empty;
+    }
+}
